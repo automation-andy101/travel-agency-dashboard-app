2679e4db767dfdbf0401093af5941e9420e47651
automation-andy101 <automation.andy101@gmail.com>
2025-05-15 22:08:16 +0100

    Started adding Trip detail page

---
+++ app/routes/admin/trip-detail.tsx
@@ -0,0 +1,34 @@
+import type {LoaderFunctionArgs} from "react-router";
+import {getTripById} from "~/appwrite/trips";
+import type { Route } from './+types/trip-detail';
+import {parseTripData} from "~/lib/utils";
+import {Header} from "../../../components";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+    const { tripId } = params;
+
+    if (!tripId) throw new Error ('Trip ID is required');
+
+    const trip = await getTripById(tripId);
+
+    return trip;
+}
+
+const TripDetail = ({ loaderData }: Route.ComponentProps) => {
+    const tripData = parseTripData(loaderData?.trip);
+
+    const { name } = tripData || {};
+
+    return (
+        <main className="travel-detail wrapper">
+            <Header title="Trip Details" description="View and Edit AI-generated travel plans" />
+
+            <section className="container wrapper-md">
+                <header>
+                    <h1 className="p-40-semi-bold text-dark-100">{name}</h1>
+                </header>
+            </section>
+        </main>
+    )
+}
+export default TripDetail
